c4a508b6b0a5a607021c57f63b131a0ad75b5f18
adisher <adilsher973@gmail.com>
2020-07-30 03:32:17 +0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Loader from './components/Loader';
 export default function App() {
   const [loading, setLoading] = useState(false)
   const [component, setComponent] = useState()
-  
+  console.log(component)
   useEffect(() => {
       setLoading(true);
 
